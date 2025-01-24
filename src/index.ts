d12489cb95ee90cbd2f0676074bf47c3b044d223
Yago Silva <yagosilvager@gmail.com>
2025-01-23 23:42:12 -0300

    3-version feat: Introduzindo a  pasta infra e fazendo a conexão com a orm prisma.

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,49 @@
 import { Categoria } from "@modules/catalogo/domain/categoria/categoria.entity";
+import { CategoriaPrismaRepository } from "@modules/catalogo/infra/database/categoria.prisma.repository";
 import { PrismaClient } from "@prisma/client";
 import { DomainException } from "@shared/domain/domain.exception";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: ['query', 'info'],
+    errorFormat: 'pretty'
+});
 
 async function main() {
-    
-    ////////////////////////
-    ///Criar Categorias////
-    ////////////////////// 
-    
-    // let categoria: Categoria;
-    // categoria = Categoria.criar({nome: "mesa"});
+    prisma.$connect().then(
+        async () => {
+            console.log('Postgres Conectado');
+        }
+    );
 
-    // ///////////////////////////////////
-    // ///Persistir Categoria no Banco////
-    // ////////////////////////////////// 
+    const categoriaRepo = new CategoriaPrismaRepository(prisma);
 
-    // await prisma.categoria.create({
-    //     data: {
-    //         id: categoria.id,
-    //         nome: categoria.nome
-    //     }
+   //const categoriaRecuperada: Categoria | null = await categoriaRepo.recuperarPorUuid("2d6e302e-0c66-42d5-9de6-48cf927a389f");
+
+    //console.log(categoriaRecuperada)
+
+    //const todasCategorias: Array<Categoria> = await categoriaRepo.recuperarTodos();
+    //console.log(todasCategorias)
+
+    //const existeCategoria: boolean = await categoriaRepo.existe("2d6e302e-0c66-42d5-9de6-48cf927a389f");
+    //console.log(existeCategoria)
+    // const categoria: Categoria = Categoria.criar({
+    //     nome:'Sala e Quarto'
+    // });
+
+    // const categoriaInserida = await categoriaRepo.inserir(categoria);
+    // console.log(categoriaInserida)
+
+    // const categoria: Categoria = Categoria.recuperar({
+    //     id: "2d6e302e-0c66-42d5-9de6-48cf927a389f",
+    //     nome: "Banho"
     // })
 
+    // const atualizouCategoria: boolean = await categoriaRepo.atualizar(categoria.id, categoria);
+    // console.log(atualizouCategoria);
 
-     ///////////////////////////////////
-    ///Atualizar Categoria no Banco////
-    ////////////////////////////////// 
-    
-    const categoriaRecuperada = await prisma.categoria.update({
-        where: {id:'2d6e302e-0c66-42d5-9de6-48cf927a389f'},
-        data: { nome: 'banho'},
-    })
-    
-    ////////////////////////
-    ///Listar Categorias///
-    //////////////////////
+    // const categoriaDeletada: boolean = await categoriaRepo.deletar("71f11c7e-abab-43d9-88bc-b7933d0ac8c4");
+    // console.log(categoriaDeletada);
 
-    const ListarCategorias = await prisma.categoria.findMany();
-    console.log(ListarCategorias);
 }
 
 main()
